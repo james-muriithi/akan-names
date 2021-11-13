1ae8a5a2d7e48bf877ea387af40cb13aea86b616
james-muriithi <muriithijames556@gmail.com>
2021-11-13 20:13:43 +0300

    gender invalid message

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 
 // UI Logic
 const akanForm = document.getElementById("akan-form");
+const numberInputs = akanForm.querySelectorAll("input[type='number']");
+const genderInputs = akanForm.querySelectorAll("input[name='gender']");
 
 akanForm.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -9,13 +11,17 @@ akanForm.addEventListener("submit", function (event) {
 });
 
 function validateForm() {
-  const numberInputs = akanForm.querySelectorAll("input[type='number']");
-  const genderInputs = akanForm.querySelectorAll("input[name='gender']");
+
   numberInputs.forEach((input) => {
       if(!input.value){
           input.classList.add('is-invalid')
       }
-  })
-  console.log(numberInputs);
-  console.log(genderInputs);
+  });
+
+  const checkedGender = akanForm.querySelectorAll("input[name='gender']:checked");
+
+  if(checkedGender.length == 0){
+    genderInputs[genderInputs.length - 1].classList.add('is-invalid')
+  }
+  
 }
