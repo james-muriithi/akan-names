5ae47ada4411c0dfe9d102ad96304b9c3ef960ba
james-muriithi <muriithijames556@gmail.com>
2021-11-13 21:06:08 +0300

    validate day of month

--- js/app.js
+++ js/app.js
@@ -7,38 +7,56 @@ const genderInputs = akanForm.querySelectorAll("input[name='gender']");
 
 akanForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  validateForm()
+  validateForm();
 });
 
 function validateForm() {
-
+  let isFormValid = true;
   numberInputs.forEach((input) => {
-      if(!input.value){
-          input.classList.add('is-invalid')
-      }
+    if (!input.value) {
+      input.classList.add("is-invalid");
+      isFormValid = false;
+    }
   });
 
-  const checkedGender = akanForm.querySelectorAll("input[name='gender']:checked");
+  const checkedGender = akanForm.querySelectorAll(
+    "input[name='gender']:checked"
+  );
 
-  if(checkedGender.length == 0){
-    genderInputs[genderInputs.length - 1].classList.add('is-invalid')
+  if (checkedGender.length == 0) {
+    genderInputs[genderInputs.length - 1].classList.add("is-invalid");
+    isFormValid = false;
   }
 
+  isFormValid = validateDay();
+  isFormValid = validateYear();
+  isFormValid = validateMonth();
+
+  return isFormValid;
 }
 
-
-function removeInputErrors(){
-    numberInputs.forEach((input) => {
-        input.addEventListener('keydown',function(){
-            this.classList.remove('is-invalid');
-        })
+function removeInputErrors() {
+  numberInputs.forEach((input) => {
+    input.addEventListener("keydown", function () {
+      this.classList.remove("is-invalid");
     });
+  });
 
-    genderInputs.forEach((input) => {
-        input.addEventListener('change',function(){
-            genderInputs[genderInputs.length - 1].remove('is-invalid');
-        })
-    })
+  genderInputs.forEach((input) => {
+    input.addEventListener("change", function () {
+      genderInputs[genderInputs.length - 1].remove("is-invalid");
+    });
+  });
 }
 
 removeInputErrors();
+
+function validateDay() {
+  const day = +document.querySelector("input#day").value;
+  if (day > 0 && day <= 31) {
+    return true;
+  }
+  document.querySelector("input#day").classList.add("is-invalid");
+  return false;
+}
+
